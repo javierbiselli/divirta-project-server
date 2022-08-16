3a13745824d1e39b8248c937bbdaf1dcd24746b9
Javier Biselli <javierbiselli@gmail.com>
2022-08-16 20:10:48 -0300

    User firebase create added

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -42,63 +42,98 @@ const getUserById = async (req, res) => {
     }
   }
 
-  const deleteUser = async (req, res) => {
-    try {
-      const user = await User.findByIdAndDelete(req.params.id);
-      if (!user) {
-        return res.status(404).json({
-          message: 'Este usuario no existe',
-          data: undefined,
-          error: true,
-        });
-      } else {
-        return res.status(200).json({
-          message: 'Usuario borrado',
-          data: undefined,
-          error: false,
-        })
-      }
-    } catch (error) {
-      return res.status(400).json({
-        message: 'Ocurrio un error, porfavor volve a intentarlo mas tarde',
-        data: error,
+const deleteUser = async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+    if (!user) {
+      return res.status(404).json({
+        message: 'Este usuario no existe',
+        data: undefined,
         error: true,
       });
+    } else {
+      return res.status(200).json({
+        message: 'Usuario borrado',
+        data: undefined,
+        error: false,
+      })
     }
+} catch (error) {
+    return res.status(400).json({
+      message: 'Ocurrio un error, porfavor volve a intentarlo mas tarde',
+      data: error,
+      error: true,
+    });
   }
+}
 
-  const updateUser = async (req, res) => {
-    try {
-    const user = await User.findByIdAndUpdate(
-    req.params.id,
-    {
+const addUser = async (req, res) => {
+  let firebaseUid;
+  try {
+    const newFirebaseUser = await firebaseApp.auth().createUser({
+      email: req.body.email,
+      password: req.body.password,
+    });
+    firebaseUid = newFirebaseUser.uid;
+
+    await firebaseApp.auth().setCustomUserClaims(newFirebaseUser.uid, { role: 'USER' });
+
+    const newUser = await User.create({
+      firebaseUid: newFirebaseUser.uid,
       name: req.body.name,
       last_name: req.body.last_name,
       email: req.body.email,
       tel: req.body.tel,
-    },
-    { new: true },);
-    if (!user) {
-      return res.status(404).json({
-        message: 'El usuario no existe',
-        data: undefined,
-        error: true,
-      });
-    }
-    return res.status(200).json({
-      message: 'Usuario actualizado',
-      data: user,
+    });
+    return res.status(201).json({
+      message: 'Usuario agregado',
+      data: newUser,
       error: false,
     });
-    } catch (error) {
-      return res.status(400).json({
-        message: 'Ocurrio un error, porfavor volve a intentarlo mas tarde',
-        data: error,
-        error: true,
-      });
+} catch (error) {
+    if (firebaseUid) {
+      await firebaseApp.auth().deleteUser(firebaseUid);
     }
+    return res.status(400).json({
+      message: error.message,
+      data: undefined,
+      error: true,
+    });
   }
+};
+
+const updateUser = async (req, res) => {
+  try {
+  const user = await User.findByIdAndUpdate(
+  req.params.id,
+  {
+    name: req.body.name,
+    last_name: req.body.last_name,
+    email: req.body.email,
+    tel: req.body.tel,
+  },
+  { new: true },);
+  if (!user) {
+    return res.status(404).json({
+      message: 'El usuario no existe',
+      data: undefined,
+      error: true,
+    });
+  }
+  return res.status(200).json({
+    message: 'Usuario actualizado',
+    data: user,
+    error: false,
+  });
+} catch (error) {
+    return res.status(400).json({
+      message: 'Ocurrio un error, porfavor volve a intentarlo mas tarde',
+      data: error,
+      error: true,
+    });
+  }
+}
 
 export default {
-    getUsers, deleteUser, updateUser, getUserById
+    getUsers, deleteUser, updateUser, getUserById, addUser
 }
