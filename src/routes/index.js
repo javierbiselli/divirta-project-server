0d22373a800d8c4eec4efc8f4b5b2df63679a5ca
Javier Biselli <javierbiselli@gmail.com>
2022-08-15 23:04:49 -0300

    Implemented firebase, users controllers, routes and model done

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,13 @@
 import express from 'express';
 import salonsRoutes from './salon'
+import auth from './auth';
+import usersRoutes from './users';
 
 const router = express.Router();
 
 router
-  .use('/salons', salonsRoutes);
+  .use('/salons', salonsRoutes)
+  .use('/users', usersRoutes)
+  .use('/auth', auth);
 
 export default router;
